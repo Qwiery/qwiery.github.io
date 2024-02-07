2c0008f32df54bc7fe2e2ed1ad693b134cc96b2e
Orbifold <Orbifold@users.noreply.github.com>
2024-02-07 10:12:57 +0000

    deploy: d456519954657feafb110dceed3be7d3e27f49f8

---
+++ assets/dal_plugins.md._y5zXZJ2.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,V as e}from"./chunks/framework.q8uJo9B6.js";const c=JSON.parse('{"title":"Plugins","description":"","frontmatter":{},"headers":[],"relativePath":"dal/plugins.md","filePath":"dal/plugins.md"}'),n={name:"dal/plugins.md"},t=e("",17),h=[t];function p(l,k,r,d,o,g){return a(),i("div",null,h)}const y=s(n,[["render",p]]);export{c as __pageData,y as default};
